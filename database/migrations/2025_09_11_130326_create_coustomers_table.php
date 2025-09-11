250ba97e5aabb0937edbe7a1f5fea43667011762
Malik Test <malekba3bad@gmail.com>
2025-09-11 16:33:32 +0300

    أول نتنkjk

---
+++ database/migrations/2025_09_11_130326_create_coustomers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('coustomers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 225);
+            $table->string('email', 225);
+            $table->string('phones', 225);
+            $table->string('address', 225);
+            $table->string('notes', 225);
+            $table->tinyInteger('active')->default(1)->comment('هل العميل مفعل ام لا');
+        
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('coustomers');
+    }
+};
