b4cba1bfb7856dfbff2596b791d69ec3d850bb44
Malik Test <malekba3bad@gmail.com>
2026-01-14 11:54:40 +0300

    CreateTraingCourse

--- app/Http/Controllers/Training_coursesController.php
+++ app/Http/Controllers/Training_coursesController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Courses;
 use Illuminate\Http\Request;
 use App\Models\training_courses;
+use App\Http\Requests\CreateTraingCourse;
+use App\Models\Training_courses as ModelsTraining_courses;
 
 class Training_coursesController extends Controller
 {
@@ -17,5 +19,24 @@ $info->course_name=Courses::where('id','=',$info->courseID)->value('name');
             }
         }
         return view('training_courses.index', compact('data'));
+    }
+
+    public function create()
+    {
+        $Courses = Courses::select('id', 'name')->where('active', 1)->get();
+        return view('training_courses.create', ['Courses' => $Courses]);
+    }
+
+     public function store(CreateTraingCourse $request){
+       
+$student = new Training_courses();
+        $student->courseID = $request->courseID;
+        $student->price = $request->price;
+        $student->start_date = $request->start_date;
+        $student->end_date = $request->end_date;
+        $student->notes = $request->notes;
+        $student->save();
+       
+        return redirect()->route('training_courses.index')->with('success','تم الإضافة بنجاح');
     }
 }
--- resources/views/training_courses/create.blade.php
+++ resources/views/training_courses/create.blade.php
@@ -1,6 +1,6 @@
 @extends("Main_layout")
 @section("title")
-  اضافة طالب
+  اضافة دورة جديدة
 @endsection
 
 @section("content")
@@ -12,78 +12,50 @@
 @endif
 
 <div class="col-md-12">
-<form method="POST" enctype="multipart/form-data" action="{{ route('student.store') }}" style="width: 80%; margin: 0 auto; background-color:white">
+<form method="POST" action="{{ route('training_courses.store') }}" style="width: 80%; margin: 0 auto; background-color:white">
     @csrf
                 <div class="card-body">
+                  
                   <div class="form-group">
-                    <label for="name">اسم الطالب</label>
-                    <input autofocus type="text" name="name" class="form-control" id="name" placeholder="اسم الطالب" value="{{ old('name') }}">
-                    @error('name')
-                    <div style="color: red" class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                  </div>
-                  <div class="form-group">
-                    <label for="country_id">الدولة</label>
-                    <select name="country_id" class="form-control select2" id="country_id">
-                        <option value="">اختر الدولة</option>
-                        @if(!empty($countries))
-                        @foreach ($countries as $info)
-                            <option value="{{ $info->id }}"> {{ $info->name }}</option>
+                    <label>الكورس المخصص للدورة</label>
+                    <select name="courseID" class="form-control select2" id="courseID">
+                        <option value="">اختر الكورس</option>
+                        @if(!empty($Courses))
+                        @foreach ($Courses as $info)
+                            <option value="{{ $info->id }}" @if(old('courseID') == $info->id) selected @endif> {{ $info->name }}</option>
                         @endforeach
                         @endif
                     </select>
-                    @error('country_id')
+                    @error('courseID')
                     <div style="color: red" class="alert alert-danger">{{ $message }}</div>
                     @enderror
                   </div>
                   <div class="form-group">
-                    <label for="address">العنوان</label>
-                    <input type="text" name="address" class="form-control" id="address" placeholder="العنوان" value="{{ old('address') }}">
-                    
+                    <label for="price">سعر الدورة</label>
+                    <input type="text" name="price" class="form-control" oninput="this.value=this.value.replace(/[^0-9.]/g,'');" id="price" placeholder="سعر الدورة" value="{{ old('price') }}">
+
                   </div>
                   <div class="form-group">
-                    <label for="phone">الهاتف</label>
-                    <input type="text" name="phone" class="form-control" id="phone" placeholder="الهاتف" value="{{ old('phone') }}">
-                    @error('phone')
+                    <label for="start_date">تاريخ بدء الدورة</label>
+                    <input type="date" name="start_date" class="form-control" id="start_date" placeholder="تاريخ بدء الدورة" value="{{ old('start_date') }}">
+                    @error('start_date')
                     <div style="color: red" class="alert alert-danger">{{ $message }}</div>
                     @enderror
                   </div>
-                  <div class="form-group">
-                    <label for="email">البريد الإلكتروني</label>
-                    <input type="email" name="email" class="form-control" id="email" placeholder="البريد الالكتروني" value="{{ old('email') }}">
-                    @error('email')
+                   <div class="form-group">
+                    <label for="end_date">تاريخ نهاية الدورة</label>
+                    <input type="date" name="end_date" class="form-control" id="end_date" placeholder="تاريخ نهاية الدورة" value="{{ old('end_date') }}">
+                    @error('end_date')
                     <div style="color: red" class="alert alert-danger">{{ $message }}</div>
                     @enderror
-                  </div>
-                <div class="form-group">
-                    <label for="nationalID">الرقم القومي</label>
-                    <input type="text" name="nationalID" class="form-control" id="nationalID" placeholder="الرقم القومي" value="{{ old('nationalID') }}">
-                    @error('nationalID')
-                    <div style="color: red" class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                  
-                  <div class="form-group">
-                    <label for="photo">الصورة</label>
-                    <input type="file" name="photo" class="form-control" id="photo" >
                   </div>
                   <div class="form-group">
                     <label for="notes">ملاحظات</label>
                     <textarea name="notes" class="form-control" id="notes" placeholder="ملاحظات">{{ old('notes') }}</textarea>
                   </div>
-                  <div class="form-group">
-                    <label for="active">حالة التفعيل</label>
-                    <select name="active" class="form-control" id="active">
-                        <option value="">اختر الحالة</option>
-                      <option value="1" @if(old('active') == 1) selected @endif>نشط</option>
-                      <option value="0" @if(old('active') == 0 and old('active')!=1) selected @endif>غير نشط</option>
-                    </select>
-                    @error('active')
-                    <div style="color: red" class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                    </div>
+                  
                    <div class="form-group" style="text-align: center; margin-bottom: 20px">
-                  <button type="submit" style="" class="btn btn-primary">Submit</button>
+                  <button type="submit" style="" class="btn btn-primary">اضف دورة</button>
                 </div>
                 </div>
                 <!-- /.card-body -->
