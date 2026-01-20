027b359fb56b9115b0009d684ef26cde91262067
Malik Test <malekba3bad@gmail.com>
2026-01-20 15:21:13 +0300

    Training_courses edit, update and delete

--- app/Http/Controllers/Training_coursesController.php
+++ app/Http/Controllers/Training_coursesController.php
@@ -39,4 +39,39 @@ $student = new Training_courses();
        
         return redirect()->route('training_courses.index')->with('success','تم الإضافة بنجاح');
     }
+
+      public function edit($id){
+        $data= Training_courses::find($id);
+        if(empty($data)){
+            return redirect()->route('training_courses.index')->with('error','الطالب غير موجود');
+        }
+        $Courses = Courses::select('id', 'name')->where('active', 1)->get();
+
+        return view('training_courses.edit',['data'=>$data,'Courses'=>$Courses]);
+    }
+
+    public function update(CreateTraingCourse $request,$id){
+        $dataTraining= Training_courses::find($id);
+        if(empty($dataTraining)){
+            return redirect()->route('training_courses.index')->with('error','الطالب غير موجود');
+        }
+        $dataTraining->courseID = $request->courseID;
+        $dataTraining->price = $request->price;
+        $dataTraining->start_date = $request->start_date;
+        $dataTraining->end_date = $request->end_date;
+        $dataTraining->notes = $request->notes;
+        $dataTraining->save();
+        return redirect()->route('training_courses.index')->with('success','تم التحديث بنجاح');
+    }
+
+    public function destroy($id){
+        $dataTraining = Training_courses::find($id);
+        if(empty($dataTraining)){
+            return redirect()->route('training_courses.index')->with('error','الطالب غير موجود');
+        }
+        $dataTraining->delete();
+        return redirect()->route('training_courses.index')->with('success','تم الحذف بنجاح');
+    }
+
+
 }
--- resources/views/training_courses/edit.blade.php
+++ resources/views/training_courses/edit.blade.php
@@ -1,6 +1,6 @@
 @extends("Main_layout")
 @section("title")
-  تعديل طالب
+تعديل دورة جديدة 
 @endsection
 
 @section("content")
@@ -12,79 +12,50 @@
 @endif
 
 <div class="col-md-12">
-<form method="POST" enctype="multipart/form-data" action="{{ route('student.update', $data->id) }}" style="width: 80%; margin: 0 auto; background-color:white">
-     @csrf
+<form method="POST" action="{{ route('training_courses.update', $data->id) }}') }}" style="width: 80%; margin: 0 auto; background-color:white">
+    @csrf
                 <div class="card-body">
+                  
                   <div class="form-group">
-                    <label for="name">اسم الطالب</label>
-                    <input autofocus type="text" name="name" class="form-control" id="name" placeholder="اسم الطالب" value="{{ old('name', $data->name) }}">
-                    @error('name')
-                    <div style="color: red" class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                  </div>
-                  <div class="form-group">
-                    <label for="country_id">الدولة</label>
-                    <select name="country_id" class="form-control select2" id="country_id">
-                        <option value="">اختر الدولة</option>
-                        @if(!empty($countries))
-                        @foreach ($countries as $info)
-                            <option value="{{ $info->id }}" @if(old('country_id', $data->country_id) == $info->id) selected @endif> {{ $info->name }}</option>
+                    <label>الكورس المخصص للدورة</label>
+                    <select name="courseID" class="form-control select2" id="courseID">
+                        <option value="">اختر الكورس</option>
+                        @if(!empty($Courses))
+                        @foreach ($Courses as $info)
+                            <option value="{{ $info->id }}" @if(old('courseID' , $data['courseID'] == $info->id)) selected @endif > {{ $info->name }}</option>
                         @endforeach
                         @endif
                     </select>
-                    @error('country_id')
+                    @error('courseID')
                     <div style="color: red" class="alert alert-danger">{{ $message }}</div>
                     @enderror
                   </div>
                   <div class="form-group">
-                    <label for="address">العنوان</label>
-                    <input type="text" name="address" class="form-control" id="address" placeholder="العنوان" value="{{ old('address', $data->address) }}">
-                    
+                    <label for="price">سعر الدورة</label>
+                    <input type="text" name="price" class="form-control" oninput="this.value=this.value.replace(/[^0-9.]/g,'');" id="price" placeholder="سعر الدورة" value="{{ old('price', $data->price) * 1 }}">
+
                   </div>
                   <div class="form-group">
-                    <label for="phone">الهاتف</label>
-                    <input type="text" name="phone" class="form-control" id="phone" placeholder="الهاتف" value="{{ old('phone', $data->phone) }}">
-                    @error('phone')
+                    <label for="start_date">تاريخ بدء الدورة</label>
+                    <input type="date" name="start_date" class="form-control" id="start_date" placeholder="تاريخ بدء الدورة" value="{{ old('start_date', $data->start_date) }}">
+                    @error('start_date')
                     <div style="color: red" class="alert alert-danger">{{ $message }}</div>
                     @enderror
                   </div>
-                  <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" name="email" class="form-control" id="email" placeholder="Email" value="{{ old('email', $data->email) }}">
-                    @error('email')
+                   <div class="form-group">
+                    <label for="end_date">تاريخ نهاية الدورة</label>
+                    <input type="date" name="end_date" class="form-control" id="end_date" placeholder="تاريخ نهاية الدورة" value="{{ old('end_date', $data->end_date) }}">
+                    @error('end_date')
                     <div style="color: red" class="alert alert-danger">{{ $message }}</div>
                     @enderror
-                  </div>
-                <div class="form-group">
-                    <label for="nationalID">الرقم القومي</label>
-                    <input type="text" name="nationalID" class="form-control" id="nationalID" placeholder="الرقم القومي" value="{{ old('nationalID', $data->nationalID) }}">
-                    @error('nationalID')
-                    <div style="color: red" class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                  
-                  <div class="form-group">
-                    <label for="photo">قم باختيار صورة</label>
-                    <img src="{{ asset('uploads/'.$data->image) }}" alt="" width="100px" height="100px" style="display: block; margin-bottom: 10px">
-                    <input type="file" name="photo" class="form-control" id="photo" >
                   </div>
                   <div class="form-group">
                     <label for="notes">ملاحظات</label>
-                    <textarea name="notes" class="form-control" id="notes" placeholder="ملاحظات">{{ old('notes' , $data->notes) }}</textarea>
+                    <textarea name="notes" class="form-control" id="notes" placeholder="ملاحظات">{{ old('notes', $data->notes) }}</textarea>
                   </div>
-                  <div class="form-group">
-                    <label for="active">حالة التفعيل</label>
-                    <select name="active" class="form-control" id="active">
-                        <option value="">اختر الحالة</option>
-                      <option value="1" @if(old('active', $data->active) == 1) selected @endif>نشط</option>
-                      <option value="0" @if(old('active', $data->active) == 0) selected @endif>غير نشط</option>
-                    </select>
-                    @error('active')
-                    <div style="color: red" class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                    </div>
+                  
                    <div class="form-group" style="text-align: center; margin-bottom: 20px">
-                  <button type="submit" style="" class="btn btn-primary">Submit</button>
+                  <button type="submit" style="" class="btn btn-primary">اضف دورة</button>
                 </div>
                 </div>
                 <!-- /.card-body -->
